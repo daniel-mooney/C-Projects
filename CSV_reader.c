94fe41fe1fb583356725bf782c9b43cdf7a56918
daniel-mooney <daniel.mooney180@gmail.com>
2021-11-14 16:06:27 +1100

    completed CSV_readline_int function

--- CSV_reader.c
+++ CSV_reader.c
@@ -1,31 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_BUFF 1000
 
-int *CSV_readline_int(FILE *csv_file);
+int *CSV_readline_int(FILE *csv_file, int length);
 void CSV_dimensions(FILE *csv_file, int *total_rows, int *total_columns);
 
 
 int main(void) {
 
     FILE *csv_fptr = fopen("./data/ordered_number_set.csv", "r");
-    
-    
 
+    int row, columns;
+    CSV_dimensions(csv_fptr, &row, &columns);
 
+    int *array = CSV_readline_int(csv_fptr, columns);
 
+    for (int i = 0; i < columns; i++) {
+        printf("%d\n", array[i]);
+    }
+
+    free(array);
     fclose(csv_fptr);
 
     return 0;
 }
 
-int *CSV_readline_int(FILE *csv_file) {
+int *CSV_readline_int(FILE *csv_file, int length) {
     
+    int *array = (int *) malloc(length * sizeof(int));
+    int i = 0;
 
+    char line[MAX_BUFF];
+    fgets(line, MAX_BUFF, csv_file);
+
+    char *token = strtok(line, ",");
+
+    while (token != NULL) {
+        array[i++] = atoi(token);
+        token = strtok(NULL, ",");
+    }
+    return array;
 }
 
 void CSV_dimensions(FILE *csv_file, int *total_rows, int *total_columns) {
+    // Finds the total rows and columns within a CSV file
 
     int rows = 0;
     int columns = 0;
@@ -43,6 +63,8 @@ void CSV_dimensions(FILE *csv_file, int *total_rows, int *total_columns) {
             }
         }
     }
-    *total_rows = rows;
-    *total_columns = columns;
+    rewind(csv_file);
+
+    *total_rows = rows + 1;
+    *total_columns = columns + 1;
 }
--- test.c
+++ test.c
@@ -4,46 +4,14 @@
 
 #define MAX_BUFF 1000
 
-void CSV_dimensions(FILE *csv_file, int *rows, int *columns);
-
 int main(void) {
 
-    FILE *fptr = fopen("./data/ordered_number_set.csv", "r");
+    char *word = " 78 ";
 
-    char line[MAX_BUFF];
-    fgets(line, MAX_BUFF, fptr);
+    int num = atoi(word);
 
-    int rows, columns;
-    CSV_dimensions(fptr, &rows, &columns);
-
-    printf("%s\n", line);
-    printf("%d, %d\n", rows, columns);
-
-
-    fclose(fptr);
+    printf("%d\n", num);
 
     return 0;
-}
-
-void CSV_dimensions(FILE *csv_file, int *total_rows, int *tota_columns) {
-
-    int rows = 0;
-    int columns = 0;
-
-    char line[MAX_BUFF];
-
-    while (fgets(line, MAX_BUFF, csv_file)) {
-        rows++;
-
-        if (columns == 0) {
-            for (int i = 0; i < strlen(line); i++) {
-                if (line[i] == ',') {
-                    columns++;
-                }
-            }
-        }
-    }
-    *total_rows = rows;
-    *tota_columns = columns + 1;
 }
 
