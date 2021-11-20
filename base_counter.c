c35646f078cb6cc1be78b8e16b1b604cffe38ad4
daniel-mooney <daniel.mooney180@gmail.com>
2021-11-21 10:30:35 +1100

    set up base_counter

---
+++ base_counter.c
@@ -0,0 +1,19 @@
+/*A program that can count from a starting to end number in any based specified (less than 10)
+without the use of standard library functions such as itoa.*/
+
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
+
+void base_counter(int start, int end, int base) {
+
+}
+
+void decimal_to_base(int decimal, int base) {
+    
+}
--- test.c
+++ test.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+void counter(int start, int end, int base);
 
 #define MAX_BUFF 1000
 
-int main(void) {
+int main(int argc, char **argv) {
+    int start = atoi(argv[1]);
+    int end = atoi(argv[2]);
+    int base = atoi(argv[3]);
 
-    char *word = " 78 ";
-
-    int num = atoi(word);
-
-    printf("%d\n", num);
+    counter(start, end, base);
 
     return 0;
+}
+
+void counter(int start, int end, int base) {
+    int n = start;
+
+    while (n <= end) {
+        char number[MAX_BUFF];
+        itoa(n, number, base);
+
+        printf("%s\n", number);
+        n++;
+    }
 }
 
