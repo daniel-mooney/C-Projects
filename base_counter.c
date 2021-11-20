8a4f26845ad3a409a00ce59bae42481a8acf0184
daniel-mooney <daniel.mooney180@gmail.com>
2021-11-21 10:42:11 +1100

    Completed decimal_to_base function

--- base_counter.c
+++ base_counter.c
@@ -4,9 +4,18 @@ without the use of standard library functions such as itoa.*/
 #include <stdio.h>
 #include <string.h>
 
+#define MAX_BUFF 1000
+
+void base_counter(int start, int end, int base);
+void decimal_to_base(int decimal, int base, char **number);
+
 
 int main(int argc, char **argv) {
+    
+    char *number;
+    decimal_to_base(83, 5, &number);
 
+    printf("%s\n", number);
     return 0;
 }
 
@@ -14,6 +23,16 @@ void base_counter(int start, int end, int base) {
 
 }
 
-void decimal_to_base(int decimal, int base) {
-    
+void decimal_to_base(int decimal, int base, char **num_str) {
+    /*Converts a decimal number to a number of a specified base (less than 10)*/
+    char number[MAX_BUFF] = "";
+
+    while (decimal > 0) {
+        int quotient = decimal % base;
+        char quot_char = quotient + '0';
+
+        strncat(number, &quot_char, 1);
+        decimal /= base;
+    }
+    *num_str = strrev(number);
 }
--- test.c
+++ test.c
@@ -1,29 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void counter(int start, int end, int base);
+#include <string.h>
 
 #define MAX_BUFF 1000
 
 int main(int argc, char **argv) {
-    int start = atoi(argv[1]);
-    int end = atoi(argv[2]);
-    int base = atoi(argv[3]);
-
-    counter(start, end, base);
-
+    char* word;
+    strcpy(word, "nice");
+    
+    printf("%s\n", word);
     return 0;
 }
-
-void counter(int start, int end, int base) {
-    int n = start;
-
-    while (n <= end) {
-        char number[MAX_BUFF];
-        itoa(n, number, base);
-
-        printf("%s\n", number);
-        n++;
-    }
-}
-
