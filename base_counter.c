7c8dffbae775f30e30e0a4e00ddb6198b72b2f2d
daniel-mooney <daniel.mooney180@gmail.com>
2021-11-21 11:43:41 +1100

    completed base_counter.c

--- base_counter.c
+++ base_counter.c
@@ -1,38 +1,60 @@
-/*A program that can count from a starting to end number in any based specified (less than 10)
+/*A program that can count from a starting to end number in any based specified (less than 11)
 without the use of standard library functions such as itoa.*/
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX_BUFF 1000
 
 void base_counter(int start, int end, int base);
-void decimal_to_base(int decimal, int base, char **number);
+int decimal_to_base(int decimal, int base, char **number);
 
 
 int main(int argc, char **argv) {
     
-    char *number;
-    decimal_to_base(83, 5, &number);
+    int start = atoi(argv[1]);
+    int end = atoi(argv[2]);
+    int base = atoi(argv[3]);
 
-    printf("%s\n", number);
+    if (base > 36) {
+        printf("base %d is to high!", base);
+        return 0;
+    }
+
+    base_counter(start, end, base);
+
     return 0;
 }
 
 void base_counter(int start, int end, int base) {
+    /*Counts and prints to terminal the numbers from start to end in what ever base specified (less than 36)*/
+    int n = start;
 
+    while (n <= end) {
+        char *number;
+        decimal_to_base(n++, base, &number);
+
+        printf("%s\n", number);
+    }
 }
 
-void decimal_to_base(int decimal, int base, char **num_str) {
-    /*Converts a decimal number to a number of a specified base (less than 10)*/
+int decimal_to_base(int decimal, int base, char **num_str) {
+    /*Converts a decimal number to a number of a specified base (less than 36)*/
     char number[MAX_BUFF] = "";
+
+    if (decimal == 0) {
+        *num_str = "0";
+        return 0;
+    }
 
     while (decimal > 0) {
         int quotient = decimal % base;
-        char quot_char = quotient + '0';
+        char quot_char = quotient < 10 ? quotient + '0' : quotient + 87;
 
         strncat(number, &quot_char, 1);
         decimal /= base;
     }
     *num_str = strrev(number);
+    return 0;
 }
--- test.c
+++ test.c
@@ -4,10 +4,23 @@
 
 #define MAX_BUFF 1000
 
+void strreverse(const char *str, char *reversed);
+
 int main(int argc, char **argv) {
-    char* word;
-    strcpy(word, "nice");
-    
-    printf("%s\n", word);
+    char *word = "racecar";
+    char reversed[MAX_BUFF];
+
+    strreverse(word, reversed);
+
+    printf("%s\n", reversed);
     return 0;
+}
+
+void strreverse(const char *str, char *reversed) {
+    int i = strlen(str) - 1;
+    int j = 0;
+    for (; i >= 0; i--, j++) {
+        reversed[j] = str[i];
+    }
+    reversed[j] = '\0';
 }
