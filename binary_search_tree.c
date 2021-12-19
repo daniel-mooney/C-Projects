add90c686dbd6426d826cffb40480c6100aa4565
daniel-mooney <daniel.mooney180@gmail.com>
2021-12-19 21:37:53 +1100

    started binary search tree

---
+++ binary_search_tree.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int value;
+
+    struct node *left;
+    struct node *right;
+};
+
+typedef struct node node;
+
+int main(void) {
+
+}
