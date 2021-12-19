8e72217119531850a5468d9a7b9874fb965f2b56
daniel-mooney <daniel.mooney180@gmail.com>
2021-12-19 22:05:53 +1100

    completed add_node

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -1,3 +1,6 @@
+/*The left side of the binary search tree gravitates towards smaller digits while the right side
+gravitates towards larger digits.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,6 +13,42 @@ struct node {
 
 typedef struct node node;
 
+// Function prototypes
+node *add_node(int value, node *base);
+
 int main(void) {
 
+}
+
+node *add_node(int value, node *base) {
+    /*Adds a node to the binary tree, returns the address of the base node.*/
+    if (base == NULL) {
+        node *new_base = (node *) malloc( sizeof(node) );
+
+        new_base ->value = value;
+        new_base ->left = NULL;
+        new_base ->right = NULL;
+
+        return new_base;
+    }
+    // Navigate through tree
+    node *new_node = (node *) malloc( sizeof(node) );
+    new_node ->value = value;
+
+    if ( base->left == NULL && base->right == NULL ) {
+        base->left = new_node;
+    } else if ( base->right->value >= value) {
+        if ( base->left == NULL ) {
+            base->left = new_node;
+        } else {
+            add_node(value, base->left);
+        }
+    } else {
+        if ( base->right == NULL) {
+            base->right = new_node;
+        } else {
+            add_node(value, base->right);
+        }
+    }
+    return base;
 }
--- test.c
+++ test.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_BUFF 1000
-
-void strreverse(const char *str, char *reversed);
-
 int main(int argc, char **argv) {
-    printf("%c\n", 3.25);
-    return 0;
+    printf("Is it a good idea to cook gnocchi in the microwave?");
+    char c;
+    scanf("%c\n", &c);
 
+    while (1) {
+        printf("hell no\n");
+    }
 }
